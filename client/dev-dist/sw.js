bb032132fa90e82535ab8f64262c0a11c5409d71
Varnie.Karmo <varnie.karmo@iga.in.gov>
2026-04-13 17:35:07 -0400

    wip

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.emks80fhb08"
+    "revision": "0.hp6eo8a0c68"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
