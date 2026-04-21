e64c495b2ff5d1e61bccee7edcfe0ec6c9963b48
Varnie.Karmo <varnie.karmo@iga.in.gov>
2026-04-21 11:38:32 -0400

    Added changes to Assignments tab

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cv4krbig8og"
+    "revision": "0.ako0v976sq8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
---
+++ client/src/store/assignmentSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { queueAction } from './syncSlice';
+
+export const assignmentSlice = createSlice({
+  name: 'assignments',
+  initialState: {
+    list: []
+  },
+  reducers: {
+    setAssignments: (state, action) => {
+      state.list = action.payload;
+    },
+    upsertAssignment: (state, action) => {
+      const index = state.list.findIndex(a => a.id === action.payload.id);
+      if (index >= 0) {
+        state.list[index] = action.payload;
+      } else {
+        state.list.push(action.payload);
+      }
+    },
+    deleteAssignment: (state, action) => {
+      state.list = state.list.filter(a => a.id !== action.payload);
+    }
+  }
+});
+
+export const { setAssignments, upsertAssignment, deleteAssignment } = assignmentSlice.actions;
+
+// Async Thunks wrapped into syncQueue mechanics for standalone mode compatibility
+export const saveAssignment = (assignmentData) => (dispatch) => {
+  dispatch(upsertAssignment(assignmentData));
+  dispatch(queueAction({ type: 'assignments/upsertAssignment', payload: assignmentData, meta: { id: Date.now() } }));
+};
+
+export const removeAssignment = (id) => (dispatch) => {
+  dispatch(deleteAssignment(id));
+  dispatch(queueAction({ type: 'assignments/deleteAssignment', payload: id, meta: { id: Date.now() } }));
+};
+
+export default assignmentSlice.reducer;
