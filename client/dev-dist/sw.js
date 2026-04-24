1622820b78f6eb28d7b0f18d4f576c7ca7acea6c
Varnie.Karmo <varnie.karmo@iga.in.gov>
2026-04-24 13:22:01 -0400

    committing changes from antigravity

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,11 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
+<<<<<<< Updated upstream
     "revision": "0.ipluorjmd7g"
+=======
+    "revision": "0.5goqj8p8ui"
+>>>>>>> Stashed changes
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
