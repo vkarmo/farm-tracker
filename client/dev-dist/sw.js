0360fa2628f64bfa63bde366ca62df9f9ea96881
Varnie N'jola Karmo <vkarmo@gmail.com>
2026-04-25 02:42:58 +0000

    wip

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,11 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
+<<<<<<< Updated upstream
     "revision": "0.unb4v61s0s8"
+=======
+    "revision": "0.01rmfr5msn"
+>>>>>>> Stashed changes
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
