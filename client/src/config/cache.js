f9de74f710720f99501d268c556f81ec2b550b3a
Varnie.Karmo <varnie.karmo@iga.in.gov>
2026-04-27 23:43:38 -0400

    Automate cache name invalidation using build time salt

--- client/src/config/cache.js
+++ client/src/config/cache.js
@@ -1,1 +1,3 @@
-export const CACHE_NAME = 'farm-tracker-v1.1';
+// __BUILD_TIME__ is injected by Vite at compile time
+const buildId = typeof __BUILD_TIME__ !== 'undefined' ? __BUILD_TIME__ : 'dev';
+export const CACHE_NAME = `farm-tracker-v1.1-${buildId}`;
